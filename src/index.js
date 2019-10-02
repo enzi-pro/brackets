b3f5634e8586c76898041ddb5fa288b0ba74f278
Nurmukhamed Zhanibek <zhanibek.n.karaganda@gmail.com>
2019-10-02 22:02:53 +0600

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let openingBracketsConfig = {};
+  let closingBracketsConfig = {};
+  for(let i = 1; i < bracketsConfig.length + 1; i++) {
+    openingBracketsConfig[bracketsConfig[i - 1][0]] = i;
+    closingBracketsConfig[bracketsConfig[i - 1][1]] = i;
+  }
+  let openingBrackets = [];
+  for(let i = 0; i < str.length; i++) {
+    if (openingBracketsConfig[str[i]]) {
+      if (closingBracketsConfig[str[i]] && openingBrackets[openingBrackets.length - 1] === closingBracketsConfig[str[i]]) {
+        openingBrackets.pop();
+      } else {
+        openingBrackets.push(openingBracketsConfig[str[i]]);
+      }      
+    } else if (closingBracketsConfig[str[i]]) {
+      if (openingBrackets.pop() === closingBracketsConfig[str[i]]) {
+        continue;
+      } else {
+        return false;
+      }    
+    } else {
+      continue;
+    }
+  }
+  if (openingBrackets.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
